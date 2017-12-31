846415b3b91ffd0ec31704f97b649038db5f4ae1
Douglas W Schultz <vxpx17@gmail.com>
2017-12-31 01:02:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function updateObjectWithKeyAndValue(obj,key,value){
   let result=Object.assign(obj,key,value);return result;}
 
 let testObj={samp:0,samp2:1};
+console.log(testObj);
 console.log(updateObjectWithKeyAndValue(testObj,'samp3',2));
