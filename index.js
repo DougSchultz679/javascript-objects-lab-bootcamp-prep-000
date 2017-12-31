965afd9df5ea95a3dcfa2ee63c880d5a8c72f9cb
Douglas W Schultz <vxpx17@gmail.com>
2017-12-31 01:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const recipes = {};
 
 function updateObjectWithKeyAndValue(obj,key,value){
-  let result=Object.assign(obj,key,value);return result;}
+  let result=Object.assign(obj,key,value);console.log(result);return result;}
 
 let testObj={samp:0,samp2:1};
-console.log(result);
 console.log(testObj);
 console.log(updateObjectWithKeyAndValue(testObj,'samp3',2));
